7748a381f1d775e19ef2d8dba31019413d6412c6
wunderhund <wunderhund@gmail.com>
2016-01-16 15:57:42 -0700

    Minor refactoring - eliminated if-then pattern logic in pickpattern()

--- passphrase.py
+++ passphrase.py
@@ -5,14 +5,12 @@
 # The script will generate <integer> passphrases. If no integer is given, it will generate 10 passphrases.
 # The wordlists included with this script were created by modifying the top 5000 most common words list from www.wordfrequency.info.
 # The Noun and Verb lists are all singular; the Nouns and Verbs lists are all plural. This improves readability.
-# In this first version only 6 patterns are implemented. Others might be devised. Be sure to remember to increase the middle number in the randrange() call if you add more.
+# In this version only 6 patterns are implemented. If you wish to add or remove a pattern, do so from the pattern= definition in pickpattern().
 # Enjoy!
 #
 
 from random import choice
-from random import randrange
 from sys import argv
-
 
 def pickwords(filename):
     """This function chooses a random word from a chosen wordlist.
@@ -27,21 +25,16 @@ def pickwords(filename):
 def pickpattern():
     """This function chooses the parts-of-speech pattern for the passphrase and returns the list"""
 
-    pattern = randrange(1, 7, 1)
+    pattern = [
+        ['Noun-common.list', 'Verbs-common.list', 'Adj-common.list', 'Nouns-common.list'],
+        ['Noun-common.list', 'Verbs-common.list', 'Nouns-common.list', 'Adverb-common.list'],
+        ['Adj-common.list', 'Nouns-common.list', 'Verb-common.list', 'Adverb-common.list'],
+        ['Noun-common.list', 'Adverb-common.list', 'Verbs-common.list', 'Noun-common.list'],
+        ['Noun-common.list', 'Adverb-common.list', 'Verbs-common.list', 'Nouns-common.list'],
+        ['Noun-common.list', 'Verbs-common.list', 'Adverb-common.list', 'Adj-common.list']
+    ]
 
-    if pattern == 1:
-        files = ['Noun-common.list', 'Verbs-common.list', 'Adj-common.list', 'Nouns-common.list']
-    elif pattern == 2:
-        files = ['Noun-common.list', 'Verbs-common.list', 'Nouns-common.list', 'Adverb-common.list']
-    elif pattern == 3:
-        files = ['Adj-common.list', 'Nouns-common.list', 'Verb-common.list', 'Adverb-common.list']
-    elif pattern == 4:
-        files = ['Noun-common.list', 'Adverb-common.list', 'Verbs-common.list', 'Noun-common.list']
-    elif pattern == 5:
-        files = ['Noun-common.list', 'Adverb-common.list', 'Verbs-common.list', 'Nouns-common.list']
-    elif pattern == 6:
-        files = ['Noun-common.list', 'Verbs-common.list', 'Adverb-common.list', 'Adj-common.list']
-    return files
+    return choice(pattern)
 
 
 def main():
